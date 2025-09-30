065fc0e909b5db5dfc32308188182a6bc6324427
Lucas Muniz <lucas.muniz.ifpr@gmail.com>
2025-09-30 17:12:25 -0300

    style(front): postcss adicionado

--- fithub-project/resources/js/components/Dashboard.jsx
+++ fithub-project/resources/js/components/Dashboard.jsx
@@ -1,12 +1,14 @@
+// fithub-project/resources/js/components/Dashboard.jsx
+
 import React from 'react';
 import { useAuth } from '../hooks/useAuth';
-import Button from './ui/Button';
+import Button from './ui/Button'; // A importação agora vai funcionar
 
 const Dashboard = () => {
   const { user, logout } = useAuth();
 
   return (
-    <div className="p-8 text-white flex flex-col items-center justify-center h-full">
+    <div className="p-8 text-black bg-white/50 rounded-lg flex flex-col items-center justify-center h-auto m-auto">
       <h1 className="text-3xl font-bold mb-4">Bem-vindo!</h1>
       {user && (
         <p className="text-lg mb-8">
